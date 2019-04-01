417ea37ec7388ccc39031aad038ccb3211e85a3e
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2019-04-01 11:04:09 +0700

    init power function template

---
+++ puzzles/power-function/power.js
@@ -0,0 +1,22 @@
+// -----
+// Run time env (tested on):
+// Node v11.12.0
+// -----
+// Algorithm complexity
+// -----
+// Run time:
+// Worst case -
+// Best case -
+// Average case -
+// -----
+// Space:
+// Worst case -
+// Best case -
+// Average case -
+// -----
+
+const power = (x, y) => {
+  return x ** y;
+};
+
+module.exports = power;
