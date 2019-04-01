c378b8009bde7afc94cbcef4460ba520f10c827a
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2019-04-01 14:56:48 +0700

    add best solution

--- puzzles/power-function/power.js
+++ puzzles/power-function/power.js
@@ -5,18 +5,41 @@
 // Algorithm complexity
 // -----
 // Run time:
-// Worst case -
-// Best case -
-// Average case -
+// Worst case - O(log(n))
+// Best case - O(1)
+// Average case - O(log(n))
 // -----
-// Space:
-// Worst case -
-// Best case -
-// Average case -
+// Space (with tail-call optimization):
+// Worst case - O(log(n))
+// Best case - O(1)
+// Average case - O(log(n))
 // -----
 
-const power = (x, y) => {
-  return x ** y;
+const power = (x, y, result = 1) => {
+  // 0 ^ y will always be 0 when y > 0
+  if (x === 0 && y > 0) {
+    return 0;
+  }
+
+  // x ^ 0 will always be 1
+  if (y === 0 && result === 1) {
+    return 1;
+  }
+
+  // if y is divisible by 2, we can divide and conquer it out
+  if (y !== 0 && (y % 2 === 0 || y % 2 === -0)) {
+    return power(x, y / 2, result) * power(x, y / 2);
+  }
+
+  if (y > 0) {
+    return power(x, y - 1, x * result);
+  }
+
+  if (y < 0) {
+    return power(x, y + 1, result / x);
+  }
+
+  return result;
 };
 
 module.exports = power;
